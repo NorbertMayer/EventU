67a66066f6f909e641597e98016029c71282de81
droidlocker <kasper.h90@outlook.dk>
2018-05-07 11:06:19 +0200

    Setup for project in Visual Studio - added Entity Framework - Setup MVC in ASP..NET

---
+++ EventU/EventU/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EventU.Startup))]
+namespace EventU
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
